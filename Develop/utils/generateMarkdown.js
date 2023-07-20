efb6498b9c55291dd7d34e11468231e030c94504
Baz Rahimi <bazrahimi@hotmail.com>
2023-07-21 01:06:48 +1000

    added other files

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,62 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
 
-`;
+// `;
+// }
+
+class MarkDown {
+  static generateReadme(answers) {
+    return `
+  # ${answers.title};
+
+  ## Table of Content
+  - [Project description](#Desription)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Test](#Test)
+  - [Questions](#Questions)
+
+  ## Description
+  ${answers.description}
+
+  ## Installation
+  ${answers.installation}
+
+  ## Usage
+  ${answers.usage}
+
+  ## License
+  ${answers.license}
+
+  ## Conbributing
+  ${answers.contributing}
+
+  ## Test
+  ${answers.test}
+
+  ## Questions
+  ${answers.email}
+  ${answers.github}
+
+    `
+
+
+  }
 }
 
 module.exports = generateMarkdown;
