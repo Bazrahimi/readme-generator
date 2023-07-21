29759905a4f4b27c434d391226efb0ea13c0c362
Baz Rahimi <bazrahimi@hotmail.com>
2023-07-21 13:26:37 +1000

    final fix to license section

--- Develop/index.js
+++ Develop/index.js
@@ -101,18 +101,65 @@ function renderLicenseChoice(license, answers) {
     case 'MIT':
       
       answers.license = ` MIT <br> This package is licensed under the MIT license, which means that anyone who uses it within your organization will be bound by the terms of the MIT license.<br> 
-        -Permission: The MIT License grants permission to anyone to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software. Users are not required to ask for permission or pay any royalties.<br> 
-        -Liability: The license comes with no warranty or liability. The software is provided "as is," and the copyright holder or contributors cannot be held liable for any damages or issues arising from its use.<br> 
-       -Attribution: The license requires that the original copyright notice and the license text must be included in all copies or substantial portions of the software.<br> 
-       -Compatibility: The MIT License is a permissive license, meaning it is compatible with other licenses. Developers can include MIT-licensed code in projects with different licenses without any conflict. <br> <br> 
-       For more information about the MIT License, [click here](https://opensource.org/licenses/MIT).`;
+      <ul>
+        <li>
+          <i>Permission:</i> The MIT License grants permission to anyone to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software. Users are not required to ask for permission or pay any royalties.
+        </li>
+        <li>
+          <i>Liability:</i> The license comes with no warranty or liability. The software is provided "as is," and the copyright holder or contributors cannot be held liable for any damages or issues arising from its use.
+        </li>
+        <li>
+          <i>Attribution:</i> The license requires that the original copyright notice and the license text must be included in all copies or substantial portions of the software.
+        </li>
+        <li>
+          <i>Compatibility:</i> The MIT License is a permissive license, meaning it is compatible with other licenses. Developers can include MIT-licensed code in projects with different licenses without any conflict.
+        </li>
+      </ul> 
+      <br> 
+      For more information about the MIT License, [click here](https://opensource.org/licenses/MIT).`;
       break;
+
+    case 'Apache-2.0':
+      answers.license = ` Apache-2.0 <br>
+      <ul>
+        <li>
+           This package is licensed under the Apache-2.0 license, which means that anyone who uses it within your organization will be bound by the terms of the Apache-2.0 license.
+        </li>
+      </ul>
+      `;
+      break;
+
+    case 'GNU-GPLv3':
+      answers.license = ` GNU-GPLv3 <br>
+      <ul>
+        <li>
+          This package is licensed under the GNU-GPLv3 license, which means that anyone who uses it within your organization will be bound by the terms of the GNU-GPLv3 license.
+        </li>
+      </ul>
+      `;
+      break;
+
+      case 'ISC':
+        answers.license = ` ISC <br>
+        <ul>
+          <li>
+             This package is licensed under the ISC license, which means that anyone who uses it within your organization will be bound by the terms of the ISC license.
+          </li>
+        </ul>
+        `;
+        break;
 
 
     case 'None':
-      answers.license = ' -None\n\nThis project does not have a specific license and is provided as-is with no warranty or support.';
+      answers.license = ` None <br>
+      <ul>
+        <li>
+              This project does not have a specific license and is provided as-is with no warranty or support.
+        </li>
+      </ul>
+      `;
       break;
-
+  
     default:
       break;
   }
--- README.md
+++ README.md
@@ -21,11 +21,22 @@
 
 ## License
  MIT <br> This package is licensed under the MIT license, which means that anyone who uses it within your organization will be bound by the terms of the MIT license.<br> 
-        -Permission: The MIT License grants permission to anyone to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software. Users are not required to ask for permission or pay any royalties.<br> 
-        -Liability: The license comes with no warranty or liability. The software is provided "as is," and the copyright holder or contributors cannot be held liable for any damages or issues arising from its use.<br> 
-       -Attribution: The license requires that the original copyright notice and the license text must be included in all copies or substantial portions of the software.<br> 
-       -Compatibility: The MIT License is a permissive license, meaning it is compatible with other licenses. Developers can include MIT-licensed code in projects with different licenses without any conflict. <br> <br> 
-       For more information about the MIT License, [click here](https://opensource.org/licenses/MIT).
+      <ul>
+        <li>
+          <i>Permission:</i> The MIT License grants permission to anyone to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software. Users are not required to ask for permission or pay any royalties.
+        </li>
+        <li>
+          <i>Liability:</i> The license comes with no warranty or liability. The software is provided "as is," and the copyright holder or contributors cannot be held liable for any damages or issues arising from its use.
+        </li>
+        <li>
+          <i>Attribution:</i> The license requires that the original copyright notice and the license text must be included in all copies or substantial portions of the software.
+        </li>
+        <li>
+          <i>Compatibility:</i> The MIT License is a permissive license, meaning it is compatible with other licenses. Developers can include MIT-licensed code in projects with different licenses without any conflict.
+        </li>
+      </ul> 
+      <br> 
+      For more information about the MIT License, [click here](https://opensource.org/licenses/MIT).
 
 
 ## Contributing
